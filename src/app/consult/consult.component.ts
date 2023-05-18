b0246a90930675301702e8411ef232e6aaaf2a60
dylandSteven <stevendylandsaldana@gmail.com>
2023-05-18 02:48:19 -0500

    get xlsx

--- src/app/consult/consult.component.ts
+++ src/app/consult/consult.component.ts
@@ -5,23 +5,30 @@ import {
 } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Consult } from '../core/services/consults';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable, catchError, retry, tap, throwError } from 'rxjs';
 import { SharedService } from '../core/services/sharedServices';
 import { response } from '../core/services/response';
+import * as XLSX from 'xlsx';
+import * as FileSaver from 'file-saver';
+import { FileSaverService } from 'ngx-filesaver';
+import * as e from 'express';
+import * as $ from 'jquery'; // import Jquery here
+
 @Component({
   selector: 'app-consult',
   templateUrl: './consult.component.html',
   styleUrls: ['./consult.component.sass'],
 })
 export class ConsultComponent {
-  constructor(private http: HttpClient, private sharedService: SharedService) {}
+  constructor(private http: HttpClient, private filerSaver: FileSaverService) {}
   message: string;
   ruc: string = '20499709944';
   fileContent: FileReader;
   file: any;
   text: string;
-  lists: string[] = new Array();
   consults: Consult[] = new Array();
+  result: response[] = new Array();
+
   API_KEY = history.state.data;
 
   httpOptions = {
@@ -31,19 +38,16 @@ export class ConsultComponent {
       Authorization: 'Bearer ' + this.API_KEY,
     }),
   };
+
   private handleError(error: HttpErrorResponse) {
     if (error.status === 0) {
-      // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error);
     } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
       console.error(
         `Backend returned code ${error.status}, body was: `,
         error.error
       );
     }
-    // Return an observable with a user-facing error message.
     return throwError(
       () => new Error('Something bad happened; please try again later.')
     );
@@ -53,22 +57,20 @@ export class ConsultComponent {
     const target = e.target as HTMLInputElement;
     this.file = (target.files as FileList)[0];
     let fileReader = new FileReader();
-    let prueba: any;
     fileReader.onload = (e) => {
-      this.finalAnswer(fileReader.result);
       fileReader.result;
+      this.finalAnswer(fileReader.result);
     };
     fileReader.readAsText(this.file);
   }
 
   finalAnswer(data: any) {
+    this.consults = [];
     this.text = data;
     const newText = data.split('\n');
-    // this.lists.push(this.text);
     if (newText[newText.length - 1] == '') {
       newText.pop();
     }
-
     newText.forEach((element: any, index: number) => {
       let newElement = element.split('|');
       let number = newElement[2].replace('M', '');
@@ -82,27 +84,47 @@ export class ConsultComponent {
           newElement[24]
         )
       );
-      console.log(this.consults);
     });
   }
 
   makeConsult() {
+    this.result = [];
+
     this.consults.forEach((element) => {
       let configUrl =
         'https://api.sunat.gob.pe/v1/contribuyente/contribuyentes/' +
         element.numRuc +
         '/validarcomprobante';
       this.http
-        .post(configUrl, element.getBody(), this.httpOptions)
-        .pipe(catchError(this.handleError))
-        .subscribe((data) => console.log(data));
+        .post<response>(configUrl, element.getBody(), this.httpOptions)
+        .pipe(retry(3), catchError(this.handleError))
+        .subscribe((data) => {
+          this.result.push(data);
+        });
     });
   }
 
-  oneConsult(configUrl: string, body: any) {
-    return this.http
-      .post<response>(configUrl, body, this.httpOptions)
-      .pipe(catchError(this.handleError))
-      .subscribe((data) => console.log(data.message));
+  exportXLSX() {
+    let final = [{}];
+    console.log(this.result);
+    console.log(this.consults);
+    this.consults.forEach((element, i) => {
+      {
+        final.push($.extend({}, JSON.parse(element.getBody()), this.result[i]));
+      }
+    });
+    const EXCEL_TYPE =
+      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    const EXCEL_EXTENSION = '.xlsx';
+
+    const worksheet = XLSX.utils.json_to_sheet(final);
+    const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
+
+    const excelBuffer: any = XLSX.write(workbook, {
+      bookType: 'xlsx',
+      type: 'array',
+    });
+    const blobData = new Blob([excelBuffer], { type: EXCEL_TYPE });
+    this.filerSaver.save(blobData, 'consultas' + EXCEL_EXTENSION);
   }
 }
